c9fb9464dab5a2385380b2548dc1660fb7570077
rskonieczny <rafal.skonieczny@atos.net>
2024-03-19 14:43:45 +0100

    sss

---
+++ numbers.py
@@ -0,0 +1,34 @@
+x = 10
+y = -5
+sum = x + y
+print(f"Sum: {sum}")
+
+
+a = 2.5
+b = 0.5
+product = a * b
+print(f"Product: {product}")
+
+
+result = 10 // 3
+print(f"Integer Division Result: {result}")
+
+
+float_result = 10.0 // 3
+print(f"Float Result from Integer Division: {float_result}")
+
+
+float_value = 3.14
+int_value = int(float_value)
+print(f"Converted Integer: {int_value}")
+
+
+int_number = 5
+float_number = float(int_number)
+print(f"Convert Float: {float_number}")
+
+
+
+
+
+
---
+++ rangesinscyling.py
@@ -0,0 +1,28 @@
+greeting = "Hello, Python!"
+sub_greeting = greeting[0:5]
+print(sub_greeting)
+
+
+language = greeting[7:]
+print(language)
+
+
+skip_chars = greeting[::2]
+print(skip_chars)
+
+
+reversed_greeting = greeting[::-1]
+print(reversed_greeting)
+
+
+last_chars = greeting[-7:]
+print(last_chars)
+
+
+middle_chars = greeting[2:-1]
+print(middle_chars)
+
+nth_chars = greeting[1:10:3]
+print(nth_chars)
+
+
