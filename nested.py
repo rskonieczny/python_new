30a082c19b9697bccb28af60f29cf169703ee900
rskonieczny <rafal.skonieczny@atos.net>
2024-03-19 11:59:25 +0100

    ddd

---
+++ nested.py
@@ -0,0 +1,13 @@
+matrix = [[1, 2, 3],[4, 5, 6], [7, 8, 9]]
+print(matrix)
+
+first_row_first_coumn = matrix[0][0]
+print(first_row_first_coumn)
+
+for row in matrix:
+    print(row)
+
+for row in matrix:
+    print(' ' .join(map(str, row)))
+
+
